cf24cc38e2f48e970b8bf37f8ad35fd668443782
Emily Chen <emily.c.chen@gmail.com>
2016-06-07 16:47:23 -0400

    Initial functionality working

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
-angular.module("myApp", [])
-	.controller("myController", function() {
-		this.title = "MadLibs";
-	});
+var app = angular.module("myApp", []);
+		app.controller("myController", function($scope) {
+			$scope.submitForm = function(isValid) {
+				if (isValid) {
+					$(".word-input").hide();
+					$(".text").show();
+				}
+			};
+
+});
