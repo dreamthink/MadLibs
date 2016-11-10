742569c449423ded2441cfc9b26673d75ad64399
dreamthinkstudios <emily.c.chen@gmail.com>
2016-11-09 22:57:14 -0500

    Refactor controller as

--- js/app.js
+++ js/app.js
@@ -1,4 +1,26 @@
 angular.module("myApp", [])
-	.controller("myController", function() {
-		
-	});
+		.controller("MyController", function() {
+			var vm = this;
+			vm.name = "name";
+			vm.jobTitle = "job title";
+			vm.tediousTask = "tedious task";
+			vm.dirtyTask = "dirty task";
+			vm.celebrity = "celebrity";
+			vm.uselessSkill = "useless skill";
+			vm.adjective = "adjective";
+			vm.obnoxiousCelebrity = "obnoxious celebrity";
+			vm.hugeNumber = 10000;
+
+	// select gender	
+		vm.gender = "";
+		vm.assignGender = function(gender) {
+			vm.gender = gender;
+		};
+
+	// on form submit, do vm
+		vm.showTextSection = false;
+
+		vm.displayMadLib = function () {
+			vm.showTextSection = true;
+		};
+});
